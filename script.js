4f2f3ddb0a0a88d3d176852ca88d327183fdc527
Matheus Barbosa <mattbarb93@gmail.com>
2021-09-13 18:54:09 -0400

    Optional Chaining

--- script.js
+++ script.js
@@ -77,21 +77,57 @@ const restaurant = {
 
 const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
 
-const matt = ['ooga', 'booga', 'fr'];
+//LOOPING OVER OBJECTS
 
-for (const [number, monkiNoise] of matt.entries()) {
-  console.log(`${number + 1}: ${monkiNoise}`);
+//<----------------------------------------------------->
+//OPTIONAL CHAINING//
+//If a certain property doesnt exist, undefined returns immediately
+
+//Without Option Chaining
+if (restaurant.openingHours && restaurant.openingHours.mon) {
+  console.log(restaurant.openingHours.mon.open);
 }
+
+//With Optional Chaining
+
+//ONLY if Monday property exists, meaning not null or undefined, it'll be displayed
+console.log(restaurant.openingHours.mon?.open);
+
+//ONLY if Opening Hours, and Monday properties exist
+console.log(restaurant.openingHours?.mon?.open);
+
+//PRACTICAL EXAMPLE
+//PRINT THE HOURS OF THE DAYS WHERE THE RESTAURANT IS OPEN
+
+const days = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
+
+for (const day of days) {
+  const open = restaurant.openingHours[day]?.open ?? 'closed'; //same as openingHours.mon
+  console.log(`On ${day}, we open at ${open}`);
+}
+
+//Methods
+console.log(restaurant.order?.(0, 1) ?? 'Method does not exist');
+console.log(restaurant.orderRisotto?.(0, 1) ?? 'Method does not exist'); //Doesnt exist
+
+//Array
+const users = [{ name: 'Jonas', email: 'hello@jonas.com' }];
+
+console.log(users[0]?.name ?? 'User array empty'); //Display if it exists. MUCH SIMPLER THAN if else, and a bunch of checks
+
+//<-------------------------------------------------->
 
 /*FOR OF LOOP
 //for (const [NAME OF INDIVIDUAL ITEM INSIDE ARRAY] of [ARRAY YOU WANNA LOOP]) {
 }
 */
-for (const item of menu) console.log(item);
+for (const item of menu) {
+  console.log(item);
+}
 
-// for (const item of menu.entries()) {
-//   console.log(`${item[0] + 1}: ${item[1]}`);
-// }
+for (const item of menu.entries()) {
+  console.log(`${item[0] + 1}: ${item[1]}`);
+}
 
 //USING DESTRUCTURING
 for (const [i, el] of menu.entries()) {
